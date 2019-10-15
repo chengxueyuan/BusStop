cb527b5a95d2464a12f95aface62f2157bab4d81
c <t>
2019-10-15 10:41:29 +0800

    1.0

--- app/src/main/java/cn/chengxueyuan/busstop/view/act/MainActivity.java
+++ app/src/main/java/cn/chengxueyuan/busstop/view/act/MainActivity.java
@@ -1,8 +1,10 @@
-package cn.chengxueyuan.busstop;
+package cn.chengxueyuan.busstop.view.act;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import cn.chengxueyuan.busstop.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
